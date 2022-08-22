c2908397e8d2d6c24589ad4d058676bf24be59a8
Christian Adrian Bernal <christian.adrian.bernal@gmail.com>
2022-08-22 18:18:20 -0300

    modis

---
+++ src/componentes/ItemListContainer.js
@@ -0,0 +1,15 @@
+import CartWidget from "./CartWidget";
+
+function ItemListContainer() {
+    return ( 
+    <>
+    <div className="item-list-container">
+    <CartWidget/>
+    <p>5</p>
+    </div>
+    
+    </>  
+    );
+}
+
+export default ItemListContainer;
